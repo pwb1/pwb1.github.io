16bdb0e409baadca4c1f8869ae4b1ef3d3fc246c
Phil Bailey <pbailey8@yahoo.com>
2018-07-11 14:35:24 -0500

    asdf

--- src/map.js
+++ src/map.js
@@ -1,8 +1,17 @@
 
-var map = d3.geomap()
-    .geofile('/d3-geomap/topojson/world/countries.json');
 
-d3.select('#map')
-    .call(map.draw, map);
-    
-window.alert("Hello");
+var map = d3.geomap.choropleth()
+    .geofile('./src/d3-geomap/topojson/countries/USA.json')
+    .projection(d3.geo.albersUsa)
+    .column('Fighters')
+    .unitId('fips')
+    .scale(800)
+    .legend(true)
+    .zoomFactor(2)
+    .colors(colorbrewer.YlGnBu[9]);
+
+d3.csv('./src/map_data.csv', function(error, data) {
+    d3.select('#map')
+        .datum(data)
+        .call(map.draw, map);
+});
